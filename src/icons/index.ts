7ffa9399c91d6e6737135c896053efb86b27dc99
Andre Hansson <andre.hansson@etimo.se>
2024-02-09 15:08:12 +0100

    Added export of new icons

--- src/icons/index.ts
+++ src/icons/index.ts
@@ -1,11 +1,17 @@
+import { BackspaceIcon } from './BackspaceIcon';
+import { CheckIcon } from './CheckIcon';
 import { ChevronDownIcon } from './ChevronDownIcon';
+import { CloseIcon } from './CloseIcon';
 import { InfoIcon } from './InfoIcon';
 import { MoonIcon } from './MoonIcon';
 import { ShuffleIcon } from './ShuffleIcon';
 import { SunIcon } from './SunIcon';
 
 export const Icon = {
+  Backspace: BackspaceIcon,
+  Check: CheckIcon,
   ChevronDown: ChevronDownIcon,
+  Close: CloseIcon,
   Info: InfoIcon,
   Moon: MoonIcon,
   Shuffle: ShuffleIcon,
