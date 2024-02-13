1be4b503b9ff1fa6a78b1a5cbd8ee2c45ec129ed
Andre Hansson <andre.hansson@etimo.se>
2024-02-13 10:43:06 +0100

    Updated desktop found word to 3 per row

--- src/components/CorrectGuessesWeb.tsx
+++ src/components/CorrectGuessesWeb.tsx
@@ -1,12 +1,13 @@
 import { FC, useMemo } from 'react';
 import { useActiveGameStore } from '../store';
 import { isPangram } from '../utils/points';
+import classNames from 'classnames';
 
 export const CorrectGuessesWeb: FC = () => {
   const { correctGuesses } = useActiveGameStore();
 
   const { rows, sortedGuesses } = useMemo(() => {
-    const rows = Math.ceil(correctGuesses.length / 4);
+    const rows = Math.ceil(correctGuesses.length / 3);
     return {
       rows,
       sortedGuesses: [...correctGuesses].sort((a, b) => (a > b ? 1 : -1))
@@ -17,33 +18,30 @@ export const CorrectGuessesWeb: FC = () => {
     <div className="flex-0 mx-20">
       <div className="bg-neutral-400 dark:bg-darkneutral-350 w-[400px] py-2 px-2 rounded-lg flex flex-col relative overflow-hidden h-[450px]">
         <h2 className="font-heading text-md text-center">Hittade ord</h2>
-        <div className="border border-darkneutral-400 rounded-xl overflow-y-scroll">
+        <div className="border border-darkneutral-400 max-h-96 overflow-y-scroll">
           {Array.from({ length: rows }).map((_, row) => {
-            const guessesInRow = sortedGuesses.slice(4 * row, 4 * row + 4);
+            const guessesInRow = sortedGuesses.slice(3 * row, 3 * row + 3);
             return (
               <div
                 key={row}
-                className="nth-[2n]:bg-darkneutral-400/50 flex gap-3"
+                className="nth-[2n]:bg-darkneutral-400/50 flex justify-evenly"
               >
                 {guessesInRow.map((guess, index) => (
-                  <div key={index} className="flex-1 px-3 py-1">
+                  <div key={index} className="py-1.5 px-2 text-center">
                     <span
-                      className={
+                      className={classNames(
                         isPangram(guess)
-                          ? 'text-purple-800 dark:text-purple-300 font-medium'
+                          ? 'text-green-800 dark:text-green-300 font-bold'
                           : ''
-                      }
+                      )}
                     >
                       {guess}
                     </span>
                   </div>
                 ))}
-
-                {guessesInRow.length !== 4 && (
+                {guessesInRow.length !== 3 && (
                   <div
-                    style={{
-                      width: `calc(100% / ${guessesInRow.length})`
-                    }}
+                    style={{ width: `calc(100% / ${guessesInRow.length})` }}
                   />
                 )}
               </div>
